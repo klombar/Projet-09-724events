39005c58533040b1d12bf8a59ce9be3fb010d921
Lombardo Kevin <kevinlombardo30@gmail.com>
2024-08-27 10:17:19 +0200

    Ajout du code permettant le filtrage des events par type

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,25 +13,20 @@ const EventList = () => {
   const { data, error } = useData()
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || [] 
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+
+  const filteredEvents = data?.events
+    .filter(event => !type || event.type === type) 
+    .slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE) || []; 
+
   const changeType = (evtType) => {
-    setCurrentPage(1);
+    setCurrentPage(1); 
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+
+  const pageNumber = Math.ceil((data?.events.filter(event => !type || event.type === type).length || 0) / PER_PAGE);
+
   const typeList = new Set(data?.events.map((event) => event.type));
+  
   return (
     <>
       {error && <div>An error occured</div>}
@@ -60,9 +55,8 @@ const EventList = () => {
             ))}
           </div>
           <div className="Pagination">
-            {[...Array(pageNumber || 0)].map((_, n) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
+            {[...Array(pageNumber)].map((_, n) => (
+              <a href="#events" onClick={() => setCurrentPage(n + 1)}>
                 {n + 1}
               </a>
             ))}
